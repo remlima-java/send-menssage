45481c537ac75b85281c7579abcb96181b3ba0be
RTI System <contato@rtisystem.dev.br>
2025-08-17 23:53:00 -0300

    feat: initialize DarkStore application with basic configuration and structure

---
+++ DarkStore/src/test/java/br/dev/rti/darkstore/DarkStoreApplicationTests.java
@@ -0,0 +1,13 @@
+package br.dev.rti.darkstore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DarkStoreApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
